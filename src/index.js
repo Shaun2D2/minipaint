614ebedbc51659e7fa6339baa8273a256050a8b1
Shaun Lobsinger <shaun@jolobo.com>
2021-06-05 13:35:07 -0700

    removed markup

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ const PAINT_RANGES = {
     citadel,
 }
 
-// get a list of paints by range
 export const getRawPaintListByRange = (range) => {
     const paints = PAINT_RANGES[range];
 
@@ -14,23 +13,18 @@ export const getRawPaintListByRange = (range) => {
     return paints;
 }
 
-// get raw paint techniques
 export const getRawTechniques = () => techniques;
 
-// fetch all paints across all the pain ranges
 export const fetchAllPaints = () => Object.entries(PAINT_RANGES).reduce((acc, [key, val]) => [...acc, ...val], []);
 
 export const fetchAllTechniques = () => getRawTechniques();
 
-// fetch an item in an array by the id prop
 export const fetchItemById = (id, arry) => {
     const item = arry.find(paint => paint.id === id);
 
     return item ?? null;
 }
 
-// fetch a paint by ID
 export const fetchPaintById = (id) => fetchItemById(id, fetchAllPaints());
 
-// fetch a technique by ID
 export const fetchTechniqueById = (id) => fetchItemById(id, fetchAllTechniques());
